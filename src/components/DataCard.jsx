1917f081f3cbe84f5df927618d2c17190171a32d
Matthew Adaro <117700776+MatthewAdaro@users.noreply.github.com>
2023-01-29 15:53:07 +0800

    Update DataCard.jsx

--- src/components/DataCard.jsx
+++ src/components/DataCard.jsx
@@ -4,26 +4,27 @@ import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
 import Typography from "@mui/material/Typography";
 import { CardActionArea } from "@mui/material";
+import StarRatings from "./StarRatings";
 
-export default function DataCard({ title }) {
+export default function DataCard({ title, category, index }) {
   return (
-    <Card sx={{ maxWidth: 345 }}>
+    <Card sx={{ maxWidth: 345 }} key={index}>
       <CardActionArea>
         <CardMedia
           component="img"
           height="140"
-          image="/static/images/cards/contemplative-reptile.jpg"
+          image="https://img.freepik.com/free-photo/flat-lay-batch-cooking-composition_23-2148765597.jpg?w=2000"
           alt="green iguana"
         />
         <CardContent>
           <Typography gutterBottom variant="h5" component="div">
-            Lizard
+            {title}
           </Typography>
           <Typography variant="body2" color="text.secondary">
-            Lizards are a widespread group of squamate reptiles, with over 6,000
-            species, ranging across all continents except Antarctica
+            {category}
           </Typography>
         </CardContent>
+        <StarRatings />
       </CardActionArea>
     </Card>
   );
